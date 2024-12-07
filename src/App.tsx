95b4339355499e3e8a078c65d10747a665d4e30a
Carlos Delfino <ibova.carlosdelfino.eti.br>
2024-12-07 00:47:08 -0300

    Ajustado Título, descritção, tawk.to e google tags.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import GroupList from './components/GroupList';
 import AdSense from './components/AdSense';
 import ScrollToTopButton from './components/ScrollToTopButton';
@@ -6,19 +7,40 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <div className="content-wrapper">
-        {/* Listagem de grupos no centro */}
-        <GroupList />
-
-        {/* Área de publicidade à direita */}
-        <AdSense />
-      </div>
-      
-      {/* Botão para retornar ao topo */}
-      <ScrollToTopButton />
-    </div>
-  );
+    <>
+      <Helmet>
+        <title>Rapport Groups</title>
+        <meta name="description" content="Indexador de Grupos da Rapport Tecnologia, envie seu grupo para ser indexado." />
+        <meta name="keywords" content="indice, indexador, index, grupo, whatsapp, divulgacao, diversos" />
+<script type="text/javascript">
+{`
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66d6862bea492f34bc0cfc26/1i6qvo6li';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 }
+`}
+</script>
+      </Helmet>
+      <div className="App">
+        <div className="content-wrapper">
+          {/* Listagem de grupos no centro */}
+          <GroupList />
+
+          {/* Área de publicidade à direita */}
+          <AdSense />
+        </div>
+
+        {/* Botão para retornar ao topo */}
+        <ScrollToTopButton />
+      </div>
+    </>
+  );
+};
 
 export default App;
